d4a1621f45605908eba6589b5a5f860b8d4d4d52
Aleksey Belchenkov <u608110@gmail.com>
2020-07-19 17:08:01 +0300

    Layout Widgets

--- lib/main.dart
+++ lib/main.dart
@@ -7,12 +7,23 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      'What is your favorite color?',
+      'What is your favorite animal?',
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('Quiz App'),
         ),
-        body: Text('This is my default text!')
+        body: Column(
+          children: <Widget>[
+            Text('The question!'),
+            RaisedButton(child: Text('Answer 1'), onPressed: null,),
+            RaisedButton(child: Text('Answer 2'), onPressed: null),
+            RaisedButton(child: Text('Answer 3'), onPressed: null),
+          ],
+        )
       ),
     );
   }
